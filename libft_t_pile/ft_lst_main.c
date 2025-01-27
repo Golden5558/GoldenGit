cab4a79289a78db8573ece03f8b36f888f07c6f6
Noe Berthalon <nberthal@k1r4p10.42mulhouse.fr>
2025-01-27 16:18:38 +0100

    fusing lst function in 2 files

--- libft_t_pile/ft_lst_main.c
+++ libft_t_pile/ft_lst_main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lst.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nberthal <nberthal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 18:18:25 by nberthal          #+#    #+#             */
-/*   Updated: 2025/01/25 01:47:23 by nberthal         ###   ########.fr       */
+/*   Created: 2025/01/27 16:08:27 by nberthal          #+#    #+#             */
+/*   Updated: 2025/01/27 16:10:10 by nberthal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,59 @@ t_pile	*ft_lstnew(int content, int index)
 	data->prev = NULL;
 	data->next = NULL;
 	return (data);
+}
+
+void	ft_lstclear(t_pile **lst)
+{
+	t_pile	*current;
+	t_pile	*next;
+
+	if (!lst || !*lst)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*lst = NULL;
+}
+
+t_pile	*ft_lstlast(t_pile *lst)
+{
+	if (!lst)
+		return (lst);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
+
+int	ft_lstsize(t_pile *lst)
+{
+	int				i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
+
+void	ft_lstadd_back(t_pile **lst, t_pile *new)
+{
+	t_pile	*prev;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		prev = ft_lstlast(*lst);
+		prev->next = new;
+		new->prev = prev;
+	}
 }
--- libft_t_pile/ft_lst_secondary.c
+++ libft_t_pile/ft_lst_secondary.c
@@ -1,16 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_lst_secondary.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nberthal <nberthal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 17:47:12 by nberthal          #+#    #+#             */
-/*   Updated: 2025/01/16 00:47:58 by nberthal         ###   ########.fr       */
+/*   Created: 2025/01/27 16:11:18 by nberthal          #+#    #+#             */
+/*   Updated: 2025/01/27 16:12:00 by nberthal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstadd_front(t_pile **lst, t_pile *new)
+{
+	if (!new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	ft_lstdelone(t_pile *lst)
+{
+	if (!lst)
+		return ;
+	free (lst);
+}
 
 void	ft_lstiter(t_pile *lst, void (*f)(int))
 {
@@ -21,4 +36,25 @@ void	ft_lstiter(t_pile *lst, void (*f)(int))
 		f(lst->content);
 		lst = lst->next;
 	}
+}
+
+t_pile	*ft_lstmap(t_pile *lst, int (*f)(int))
+{
+	t_pile	*ret;
+	t_pile	*tmp;
+	int		tmp2;
+
+	if (!lst || !f)
+		return (NULL);
+	ret = NULL;
+	while (lst)
+	{
+		tmp2 = f(lst->content);
+		tmp = ft_lstnew(tmp2, 0);
+		if (!tmp)
+			return (ft_lstclear(&ret), NULL);
+		ft_lstadd_back(&ret, tmp);
+		lst = lst->next;
+	}
+	return (ret);
 }
